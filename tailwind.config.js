0921e258b85573b04b829767c89fcdf4b2bc14fa
JK <mletemps@hotmail.com>
2025-09-03 08:51:00 -0400

    updated themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./pb_hooks/pages/**/*.{ejs,md}'],
-  theme: {
-    extend: {},
-  },
-  daisyui: {
-    themes: ['cupcake'],
-  },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+    content: ['./pb_hooks/pages/**/*.{ejs,md}'],
+    theme: {
+        extend: {},
+    },
+    daisyui: {
+        themes: ['nord', 'dark'],
+        darkTheme: 'dark',
+        base: true, // applies background color and foreground color
+        styled: true, // include daisyUI colors and design decisions
+        utils: true, // adds responsive and modifier utility classes
+    },
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
