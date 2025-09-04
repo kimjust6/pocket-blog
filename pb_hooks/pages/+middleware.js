5c5152099923f98547ab82075658f19a27b69f68
JK <mletemps@hotmail.com>
2025-09-04 18:33:21 -0400

    minor tweaks to navbar

--- pb_hooks/pages/+middleware.js
+++ pb_hooks/pages/+middleware.js
@@ -1,10 +1,4 @@
 /** @type {import('pocketpages').MiddlewareLoaderFunc} */
 module.exports = function (api) {
-    return {
-        siteName: "Justin's Blog",
-        navigation: [
-            // { title: 'Home', url: '/' },
-            { title: 'My Blog Posts', url: '/blog/posts' },
-        ],
-    }
+    return {}
 }
