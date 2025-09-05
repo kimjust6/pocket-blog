bc31c32e310d38b2ea18a67c71b38a899d74c562
JK <mletemps@hotmail.com>
2025-09-05 00:52:01 -0400

    added some random og meta tags

--- pb_hooks/pages/+middleware.js
+++ pb_hooks/pages/+middleware.js
@@ -1,4 +1,39 @@
 /** @type {import('pocketpages').MiddlewareLoaderFunc} */
 module.exports = function (api) {
-    return {}
+    return {
+        metadata: [
+            // Basic metadata
+            { name: 'title', content: 'Snapshots of My Thoughts' },
+            {
+                name: 'description',
+                content:
+                    'Unfiltered personal reflections and insights—straight from the mind behind the console.',
+            },
+            { name: 'url', content: 'https://blog.jkim.win/' },
+
+            // Open Graph metadata
+            { name: 'og:title', content: 'Snapshots of My Thoughts' },
+            { name: 'og:type', content: 'website' },
+            { name: 'og:url', content: 'https://blog.jkim.win/' },
+            {
+                name: 'og:image',
+                content: 'https://blog.jkim.win/og-image.png',
+            },
+            {
+                name: 'og:description',
+                content:
+                    'Unfiltered personal reflections and insights—straight from the mind behind the console.',
+            },
+            { name: 'og:site_name', content: "Justin's Blog" },
+            { name: 'og:locale', content: 'en_CA' },
+
+            // Author / article-specific
+            {
+                name: 'article:author',
+                content: 'https://www.linkedin.com/in/jkim',
+            },
+            { name: 'article:author:name', content: 'Justin Kim' },
+            { name: 'article:publisher', content: 'https://www.justink.dev/' },
+        ],
+    }
 }
