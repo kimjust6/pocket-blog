352dc35a61db656afa2e522f28cbf89cdc119735
Justin Kim <mletemps@hotmail.com>
2025-09-27 10:18:21 -0400

    wip

--- pb_hooks/pages/utils/common.js
+++ pb_hooks/pages/utils/common.js
@@ -1,8 +1,11 @@
 function formatDateTime(date) {
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const month = months[date.getMonth()];
     const day = date.getDate().toString().padStart(2, '0');
     const year = date.getFullYear();
-    return `${month}/${day}/${year}`;
+    return `${month} ${day}, ${year}`;
 }
 
 function getImageUrl(blog) {
