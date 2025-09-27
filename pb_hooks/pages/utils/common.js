ec2c95936132ddff789771c5754d7a65c12a1019
Justin Kim <mletemps@hotmail.com>
2025-09-27 10:22:13 -0400

    prettier/linting

--- pb_hooks/pages/utils/common.js
+++ pb_hooks/pages/utils/common.js
@@ -1,18 +1,33 @@
 function formatDateTime(date) {
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+    ]
 
-    const month = months[date.getMonth()];
-    const day = date.getDate().toString().padStart(2, '0');
-    const year = date.getFullYear();
-    return `${month} ${day}, ${year}`;
+    const month = months[date.getMonth()]
+    const day = date.getDate().toString().padStart(2, '0')
+    const year = date.getFullYear()
+    return `${month} ${day}, ${year}`
 }
 
 function getImageUrl(blog) {
-    return `/api/files/${blog.collectionId}/${blog.id}/${blog.coverImage}` || 'https://images.unsplash.com/photo-1518373714866-3f1478910cc0?w=600&h=400&fit=crop';
+    return (
+        `/api/files/${blog.collectionId}/${blog.id}/${blog.coverImage}` ||
+        'https://images.unsplash.com/photo-1518373714866-3f1478910cc0?w=600&h=400&fit=crop'
+    )
 }
 
 module.exports = {
     formatDateTime,
     getImageUrl,
-};
+}
