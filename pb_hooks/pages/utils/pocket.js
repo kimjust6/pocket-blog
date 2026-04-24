214c4089e1ae281010a7a183cf50c050fd9e563a
Justin Kim <mletemps@hotmail.com>
2026-04-23 23:44:17 -0400

    updated the order list by manual publish date

--- pb_hooks/pages/utils/pocket.js
+++ pb_hooks/pages/utils/pocket.js
@@ -8,7 +8,7 @@ getBlogPostById = (request, id) => {
         .collection(POCKET_BLOGPOSTS)
         .getFirstListItem(
             `id = "${id}" && isDeleted = false`,
-            { sort: '-updated' }
+            { sort: '-manualPublishDate,-updated' }
         )
 }
 
